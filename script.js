2dcc1586d3609bc5d3157800a22fc7a6b5749df1
endulum <apheum@zoho.com>
2022-11-27 12:23:20 -0800

    number button functionality, display functionality

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+let display = ["0"];
+
+function updateDisplay() {
+    const toDisplay = display.join('');
+    document.getElementById('display').textContent = toDisplay;
+}
+
+function extractNumber(event) {
+    let num = event.srcElement.id;
+    num = num.charAt(num.length - 1);
+    if (display[0] == 0) {
+        display[0] = num;
+        updateDisplay();
+    } else {
+        display.push(num);
+        updateDisplay();
+    }
+}
+
+document.querySelectorAll('.number').forEach(item => {
+    item.addEventListener('click', extractNumber);
+})
+
 function add(a, b) {
     return a + b
 } function subtract(a, b) {
@@ -8,6 +31,6 @@ function add(a, b) {
     return a / b
 }
 
-function operate(operation, a, b) {
-    return operation(a, b);
-}
+// function operate(operation, a, b) {
+//     return operation(a, b);
+// }
