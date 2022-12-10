767e0b62011ec69190e8c1a10c7baf67c0665291
endulum <apheum@zoho.com>
2022-12-09 18:50:17 -0800

    lots of ifs, decimal wrangling

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let memory = {
 };
 
 let inputFlag = false;
+let evaluationFlag = false;
 
 const display = document.getElementById('display');
 function updateDisplay(textcontent) {
@@ -15,22 +16,34 @@ updateDisplay(memory.current.join(''));
 
 document.querySelectorAll('.number').forEach(item => {
     item.addEventListener('click', (event) => {
+
+        if (evaluationFlag == true) {
+            wipeMemory(memory);
+            evaluationFlag = false;
+        };
+        
         if (event.target.id == 'decimal') {
-            memory.current.push('.');
+            if (memory.current.includes('.') != true) {
+                memory.current.push('.');
+                console.log(memory.current.join(''));
+            }
         } else {
             let num = event.target.id.charAt(event.target.id.length - 1);
-            if (memory.current.join('') == "0") {
+            if (memory.current.join('') === "0") {
                 memory.current[0] = num;
             } else {
                 memory.current.push(num);
             };
-        }; if (inputFlag == false) {inputFlag = true;};
+        }; 
+        
+        if (inputFlag == false) {inputFlag = true;};
         updateDisplay(memory.current.join(''));
     });
 });
 
 document.querySelectorAll('.operation').forEach(item => {
     item.addEventListener('click', (event) => {
+        if (evaluationFlag == true) {evaluationFlag = false;};
         if (inputFlag == true) {
             if (memory.previous == '') {
                 memory.previous = parseFloat(memory.current.join(''));
@@ -56,6 +69,7 @@ document.getElementById('equals').addEventListener('click', () => {
     if (inputFlag == true) {
         memory.current = parseFloat(memory.current.join(''));
         updateDisplay(evaluation(memory));
+        evaluationFlag = true;
     };
 });
 
